51f7208063a1734ae7a8e6a9fee00757892bee09
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2022-02-16 14:00:43 +0100

    base de datos

---
+++ kalpataru_v2.sql
@@ -0,0 +1,243 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.18 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para kalpataru_v2
+DROP DATABASE IF EXISTS `kalpataru_v2`;
+CREATE DATABASE IF NOT EXISTS `kalpataru_v2` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `kalpataru_v2`;
+
+-- Volcando estructura para tabla kalpataru_v2.clases
+DROP TABLE IF EXISTS `clases`;
+CREATE TABLE IF NOT EXISTS `clases` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `idCurso` bigint(20) unsigned NOT NULL,
+  `idNivel` bigint(20) unsigned NOT NULL,
+  `idGrado` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `clases_idcurso_foreign` (`idCurso`),
+  KEY `clases_idnivel_foreign` (`idNivel`),
+  KEY `clases_idgrado_foreign` (`idGrado`),
+  CONSTRAINT `clases_idcurso_foreign` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`id`),
+  CONSTRAINT `clases_idgrado_foreign` FOREIGN KEY (`idGrado`) REFERENCES `grados` (`id`),
+  CONSTRAINT `clases_idnivel_foreign` FOREIGN KEY (`idNivel`) REFERENCES `niveles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.cursos
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `numero` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.failed_jobs
+DROP TABLE IF EXISTS `failed_jobs`;
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.grados
+DROP TABLE IF EXISTS `grados`;
+CREATE TABLE IF NOT EXISTS `grados` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abreviacion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.megusta
+DROP TABLE IF EXISTS `megusta`;
+CREATE TABLE IF NOT EXISTS `megusta` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `idUsuario` bigint(20) unsigned NOT NULL,
+  `idMensaje` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `megusta_idusuario_foreign` (`idUsuario`),
+  KEY `megusta_idmensaje_foreign` (`idMensaje`),
+  CONSTRAINT `megusta_idmensaje_foreign` FOREIGN KEY (`idMensaje`) REFERENCES `mensajes` (`id`),
+  CONSTRAINT `megusta_idusuario_foreign` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.mensajeestados
+DROP TABLE IF EXISTS `mensajeestados`;
+CREATE TABLE IF NOT EXISTS `mensajeestados` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.mensajes
+DROP TABLE IF EXISTS `mensajes`;
+CREATE TABLE IF NOT EXISTS `mensajes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `texto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_creacion` date NOT NULL,
+  `meGustas` bigint(20) unsigned NOT NULL,
+  `formahoja` bigint(20) unsigned NOT NULL,
+  `hexadecimalColorHoja` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idUsuario` bigint(20) unsigned NOT NULL,
+  `idClase` bigint(20) unsigned NOT NULL,
+  `idEstado` bigint(20) unsigned NOT NULL,
+  `anonimo` bigint(20) unsigned NOT NULL,
+  `tokenActivacion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenEliminar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mensajes_idusuario_foreign` (`idUsuario`),
+  KEY `mensajes_idclase_foreign` (`idClase`),
+  KEY `mensajes_idestado_foreign` (`idEstado`),
+  CONSTRAINT `mensajes_idclase_foreign` FOREIGN KEY (`idClase`) REFERENCES `clases` (`id`),
+  CONSTRAINT `mensajes_idestado_foreign` FOREIGN KEY (`idEstado`) REFERENCES `mensajeestados` (`id`),
+  CONSTRAINT `mensajes_idusuario_foreign` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.migrations
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.niveles
+DROP TABLE IF EXISTS `niveles`;
+CREATE TABLE IF NOT EXISTS `niveles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abreviacion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.palabrasprohibidas
+DROP TABLE IF EXISTS `palabrasprohibidas`;
+CREATE TABLE IF NOT EXISTS `palabrasprohibidas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `palabra` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.password_resets
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) unsigned DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT 'users/default.png',
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `settings` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  KEY `users_role_id_foreign` (`role_id`),
+  CONSTRAINT `users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.usuarioestados
+DROP TABLE IF EXISTS `usuarioestados`;
+CREATE TABLE IF NOT EXISTS `usuarioestados` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.usuarioroles
+DROP TABLE IF EXISTS `usuarioroles`;
+CREATE TABLE IF NOT EXISTS `usuarioroles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla kalpataru_v2.usuarios
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dni` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contrasenia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `idClase` bigint(20) unsigned NOT NULL,
+  `idEstado` bigint(20) unsigned NOT NULL,
+  `idRol` bigint(20) unsigned NOT NULL,
+  `penalizaciones` bigint(20) unsigned NOT NULL,
+  `tokenActivacion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenRecordarpass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarios_idclase_foreign` (`idClase`),
+  KEY `usuarios_idestado_foreign` (`idEstado`),
+  KEY `usuarios_idrol_foreign` (`idRol`),
+  CONSTRAINT `usuarios_idclase_foreign` FOREIGN KEY (`idClase`) REFERENCES `clases` (`id`),
+  CONSTRAINT `usuarios_idestado_foreign` FOREIGN KEY (`idEstado`) REFERENCES `usuarioestados` (`id`),
+  CONSTRAINT `usuarios_idrol_foreign` FOREIGN KEY (`idRol`) REFERENCES `usuarioroles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
